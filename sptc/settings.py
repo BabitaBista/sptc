443f671d27d12d66561ceeb4e1c079c77e009dcc
Babita Bista <bistababita903@gmail.com>
2023-05-08 22:45:52 +0545

    refactor code

--- sptc/settings.py
+++ sptc/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)imv(c(^*x2b1=s$c+gq)r0oxq)wrm9j&c)x7qgzb=pl%+8r6*'
+# SECRET_KEY = 'django-insecure-)imv(c(^*x2b1=s$c+gq)r0oxq)wrm9j&c)x7qgzb=pl%+8r6*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
